1adcf795cf5e101cb0ced9695a4fde0c0d7ddfb6
Rayasxw <maksatbekovarayana@gmail.com>
2025-01-08 10:12:04 +0600

    DZ2

--- src/App.js
+++ src/App.js
@@ -1,45 +1,16 @@
 
 import './App.css';
+import MainPage from "./pages/mainPage/MainPage";
 
 
-function Header () {
-    return (
-        <div>
-            <h2>Header</h2>
-            <Title/>
-        </div>
-    )
-}
 
-function Footer() {
-    return (
-        <div>
-            <h2>Footer</h2>
-            <Title/>
-        </div>
-    )
-}
-function Content() {
-    return (
-        <div>
-            <h2>Content</h2>
-            <Title/>
-        </div>
-    )
-}
-function Title() {
-    return (
-        <div>
-            <p>Title</p>
-        </div>
-    )
-}
+
+
+
 function App() {
   return (
     <div className="App">
-        <Header/>
-        <Content/>
-        <Footer/>
+        <MainPage/>
     </div>
   );
 }
---
+++ src/components/title/Title.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Title({text}) {
+    return (
+        <div>
+            <h3>{text}</h3>
+        </div>
+    );
+}
+
+export default Title;
